703ff800477f35d0b17acace80af1b189e2e028f
baianoo-cmd <enzopipoca00@gmail.com>
2026-08-22 09:45:57 -0300

    feat: added function to delete products by ID

--- main.c
+++ main.c
@@ -7,14 +7,35 @@ int main(){
 
     int tamanho = 0;
     int capacidade = 0;
+    int idParaDeletar, n;
 
     cadastrarProduto(&produtos, &tamanho, &capacidade);
     cadastrarProduto(&produtos, &tamanho, &capacidade);
     cadastrarProduto(&produtos, &tamanho, &capacidade);
 
     printf("\n===== PRODUTOS =====\n");
-
     listarProdutos(produtos, tamanho, 0);
+
+
+    printf("Deseja deletar algum produto?\n[1] para sim | [0] para nao\n");
+    scanf("%d", &n);
+
+    if (n == 1) {
+        printf("Digite o ID do produto que deseja deletar: ");
+        scanf("%d", &idParaDeletar);
+
+        int resultado = deletarProduto(produtos, &tamanho, idParaDeletar);
+
+        if (resultado == 1) {
+            printf("Produto deletado com sucesso!\n");
+        }   else    {
+            printf("Produto nao encontrado!\n");
+        }
+
+        printf("\n===== PRODUTOS APOS A REMOCAO =====\n");
+
+        listarProdutos(produtos, tamanho, 0);
+    }
 
     free(produtos);
 
